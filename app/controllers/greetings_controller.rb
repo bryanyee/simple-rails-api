6c6c1c982dfbe9966ac3f35d9e23656942118fdc
Bryan Yee <yee.bryan.c@gmail.com>
2022-01-12 19:40:49 -0800

    Add users controller and model
    rails generate scaffold Users first_name:string lastName:string
    
    rails db:migrate

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,7 @@
 # rails generate controller Greetings hello
 class GreetingsController < ApplicationController
   def hello
-    data = { num: 5, str: 'hello' }
+    data = { num: 5, str: 'hello', pet: Pet.first }
     render json: data
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_021253) do
+ActiveRecord::Schema.define(version: 2022_01_13_025933) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
