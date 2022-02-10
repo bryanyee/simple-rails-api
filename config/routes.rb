9397c762fb0f044ccd4f69a77a197ffa8a7c57a4
Bryan Yee <yee.bryan.c@gmail.com>
2022-02-10 11:30:43 -0800

    Add status endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  get '/status', to: ->(env) { [200, {}, ['ok']] }
+
   resources :users do
     resources :posts, only: %i(index create)
   end
