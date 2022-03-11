2c0341a8171dc6a3d507a207cce7086f83608d61
Bryan Yee <yee.bryan.c@gmail.com>
2022-03-11 12:19:54 -0800

    Fix controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,7 +12,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { firstName: @user.firstName, lastName: @user.lastName } }, as: :json
+      post users_url, params: { user: { first_name: @user.first_name, last_name: @user.last_name } }, as: :json
     end
 
     assert_response 201
@@ -24,7 +24,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { firstName: @user.firstName, lastName: @user.lastName } }, as: :json
+    patch user_url(@user), params: { user: { first_name: @user.first_name, last_name: @user.last_name } }, as: :json
     assert_response 200
   end
 
